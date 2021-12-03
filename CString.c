4fd4ececa3963aa7fdb28eb90f7c6dd80f0a4947
Maria Markowiak <maria.markowiak@gmail.com>
2021-12-03 23:56:14 +0100

    cstring_deallocate function

--- CString.c
+++ CString.c
@@ -2,23 +2,37 @@
 
 
 CString* cstring_new(const char* str) {
+
     CString* cstring;
     cstring = malloc(sizeof(CString));
+    cstring->length = strlen(str);
+    cstring->string = malloc((cstring->length + 1) * sizeof(char)); //extra character at the end of string (\0)
+    strcpy(cstring->string, str);
 
+    return cstring;
 }
 
-int cstring_length() {
-    return 1;
+void cstring_deallocate(CString* cstring) {
+    free(cstring->string);
+    free(cstring);
 }
 
-char cstring_get_char_at(CString cstring, int index) {
-    return 'a';
+int cstring_length(CString* cstring) {
+    return cstring->length;
 }
 
-int cstring_set_char_at(CString cstring, int index, char ch) {
+char cstring_get_char_at(CString* cstring, int index) {
+    if(index < 0 || index >= cstring->length) return -1;
+    return cstring->string[index];
+}
+
+int cstring_set_char_at(CString* cstring, int index, char ch) {
+    if(index < 0 || index >= cstring->length) return -1;
+  
+    cstring->string[index] = ch;
     return 0;
 }
 
 const char* cstring_get_string(CString* cstring) {
-    return 'a';
+    return cstring->string;
 }
--- CString.h
+++ CString.h
@@ -1,3 +1,6 @@
+#ifndef CSTRING_H
+#define CSTRING_H
+
 typedef struct CString {
 
     char *string;
@@ -6,8 +9,11 @@ typedef struct CString {
 } CString;
 
 CString* cstring_new(const char* str);
+void cstring_deallocate(CString* cstring);
 
 int cstring_length();
-char cstring_get_char_at(CString cstring, int index);
-int cstring_set_char_at(CString cstring, int index, char ch);
+char cstring_get_char_at(CString* cstring, int index);
+int cstring_set_char_at(CString* cstring, int index, char ch);
 const char* cstring_get_string(CString* cstring);
+
+#endif
