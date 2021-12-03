40a7cb581a4a0d7907a09c68e1083b574c015f36
Maria Markowiak <maria.markowiak@gmail.com>
2021-12-04 00:37:41 +0100

    add comments to functions

--- CString.c
+++ CString.c
@@ -1,38 +1,37 @@
 #include "CString.h"
 
+CString *cstring_new(const char *str) {
 
-CString* cstring_new(const char* str) {
-
-    CString* cstring;
+    CString *cstring;
     cstring = malloc(sizeof(CString));
     cstring->length = strlen(str);
-    cstring->string = malloc((cstring->length + 1) * sizeof(char)); //extra character at the end of string (\0)
+    cstring->string = malloc((cstring->length + 1) * sizeof(char)); /* an extra character at the end of string (\0) */
     strcpy(cstring->string, str);
 
     return cstring;
 }
 
-void cstring_deallocate(CString* cstring) {
+void cstring_deallocate(CString *cstring) {
     free(cstring->string);
     free(cstring);
 }
 
-int cstring_length(CString* cstring) {
+int cstring_length(CString *cstring) {
     return cstring->length;
 }
 
-char cstring_get_char_at(CString* cstring, int index) {
+char cstring_get_char_at(CString *cstring, int index) {
     if(index < 0 || index >= cstring->length) return -1;
     return cstring->string[index];
 }
 
-int cstring_set_char_at(CString* cstring, int index, char ch) {
+int cstring_set_char_at(CString *cstring, int index, char ch) {
     if(index < 0 || index >= cstring->length) return -1;
   
     cstring->string[index] = ch;
     return 0;
 }
 
-const char* cstring_get_string(CString* cstring) {
+const char *cstring_get_string(CString *cstring) {
     return cstring->string;
 }
--- CString.h
+++ CString.h
@@ -1,6 +1,11 @@
 #ifndef CSTRING_H
 #define CSTRING_H
 
+/* 
+* CString struct contains:
+* - a pointer to a char array
+* - a length of a string (char array) 
+*/
 typedef struct CString {
 
     char *string;
@@ -8,12 +13,22 @@ typedef struct CString {
 
 } CString;
 
-CString* cstring_new(const char* str);
-void cstring_deallocate(CString* cstring);
+/* returns newly created CString struct */
+CString *cstring_new(const char *str);
 
-int cstring_length();
-char cstring_get_char_at(CString* cstring, int index);
-int cstring_set_char_at(CString* cstring, int index, char ch);
-const char* cstring_get_string(CString* cstring);
+/* deallocates memory taken by a given CString struct */
+void cstring_deallocate(CString *cstring);
+
+/* returns length of a string (char array) in CString struct */
+int cstring_length(CString *cstring);
+
+/* returns a char from array at a given index */
+char cstring_get_char_at(CString *cstring, int index);
+
+/* set a given char at a specified index */
+int cstring_set_char_at(CString *cstring, int index, char ch);
+
+/* returns a pointer to a string (char array) in a given CString struct */
+const char *cstring_get_string(CString *cstring);
 
 #endif
